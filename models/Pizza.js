fe673f4ddde91c98a61a364c1705de24f83178a5
geicibarham <geicianecosta43@gmail.com>
2022-04-25 13:31:59 -0400

    crud for comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
 
 const PizzaSchema = new Schema({
 
@@ -11,18 +12,37 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
-})
+    toppings: [],
 
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+
+    }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 // create pizza model using pizzaSchema
 
-const Pizza = model('Pizza' , PizzaSchema);
+const Pizza = model('Pizza', PizzaSchema);
+
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
+})
 
 // export pizza model
 module.exports = Pizza;
